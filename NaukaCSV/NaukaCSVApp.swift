e943e1df5aec0a822d54494c1ff6201ce8aff6e0
Adam Sidor <sidoadsi1@gmail.com>
2026-06-14 17:27:03 +0200

    Initial Commit

---
+++ NaukaCSV/NaukaCSVApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaukaCSVApp.swift
+//  NaukaCSV
+//
+//  Created by Adam Sidor on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct NaukaCSVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
